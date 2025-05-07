cc2ec219fff50daba5e2374e98772e8809ae3902
chan <heechan.log@gmail.com>
2025-05-07 19:02:35 +0900

    방향으로 이동하는 코드를 Direction 클래스 에서 Position 클래스로 변경

--- src/main/java/com/example/demo/domain/common/Position.java
+++ src/main/java/com/example/demo/domain/common/Position.java
@@ -1,9 +1,25 @@
 package com.example.demo.domain.common;
 
+import java.util.Objects;
+
 public record Position(int x, int y) {
 
     public Position move(int dx, int dy) {
         return new Position(this.x + dx, this.y + dy);
     }
 
+    public Position moveIndirection(Direction direction, int step) {
+        Objects.requireNonNull(direction, "Direction cannot be null");
+
+        if (step <= 0) {
+            throw new IllegalStateException("Step must be a positive(step <= 0) integer, but was " + step);
+        }
+
+        return switch (direction) {
+            case UP -> this.move(0, -step);
+            case DOWN -> this.move(0, step);
+            case LEFT -> this.move(-step, 0);
+            case RIGHT -> this.move(step, 0);
+        };
+    }
 }
